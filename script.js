f7f49f6733888d05323ce8b053b9c5b2db8d1d30
Vishal.AI <132605163+vishalP-08@users.noreply.github.com>
2023-08-18 17:07:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function getFormvalue() {
-    //Write your code here
-
+   // Get the form element by its ID
+    var form = document.getElementById("myForm");
+    
+    // Get the values of the first and last name fields
+    var firstName = form.elements["first-name"].value;
+    var lastName = form.elements["last-name"].value;
+    
+    // Display the values using alert
+    alert("First Name: " + firstName + "\nLast Name: " + lastName);
 }
+
+// Attach the function to the form's submit event
+var form = document.getElementById("myForm");
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    getFormValue(); // Call the function to display the values
+});
+
